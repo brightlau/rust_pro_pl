feb72e23a1d346bc3ba9fc5bc0ae6b2a3c8f7c92
brightl <brightl3016@outlook.com>
2025-06-07 12:39:12 +0800

    rust println! 宏

--- guess_game/src/lib.rs
+++ guess_game/src/lib.rs
@@ -3,6 +3,7 @@ mod guess;
 use guess::core::guess_fn;
 
 pub fn guessing() {
-    println!("beg, guessing!");
+    println!("beg, guessing...");
     guess_fn();
+    println!("end, guessing...")
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod manager;
 
 use manager::game::game;
 
+// 程序入口函数 fn 声明函数， () 参数， {} 函数体
 fn main() {
-    println!("Hello, world!");
+    
+    // 猜字游戏库
     game();
 }
